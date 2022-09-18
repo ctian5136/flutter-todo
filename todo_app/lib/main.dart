891a0b85a14420bedcaf1d31e25f05a193684860
cindytian <55060072+ctian5136@users.noreply.github.com>
2022-09-17 20:50:38 -0400

    todo into flutter

---
+++ todo_app/lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Todo());
+}
+
+class Todo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: TodoList());
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  // save data
+  final List<String> _todoList = <String>[];
+  // text field
+  final TextEditingController _textFieldController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('To-Do List')),
+      body: ListView(children: _getItems()),
+      // add items to the to-do list
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _displayDialog(context),
+          tooltip: 'Add Item',
+          child: Icon(Icons.add)),
+    );
+  }
+
+  void _addTodoItem(String title) {
+    // Wrapping it inside a set state will notify
+    // the app that the state has changed
+    setState(() {
+      _todoList.add(title);
+    });
+    _textFieldController.clear();
+  }
+
+  // this Generate list of item widgets
+  Widget _buildTodoItem(String title) {
+    bool isChecked = false;
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+    return ListTile(title: Text(title),
+    trailing: Column(
+                children: [
+                    InkWell(
+                    onTap: () {
+                      //call your onpressed function here
+                      print('Button Pressed');
+                    },
+                    child: Icon(Icons.edit),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                ]
+            
+    ),
+    leading: Column(
+      children: [
+        Checkbox(
+          checkColor: Colors.white,
+          fillColor: MaterialStateProperty.resolveWith(getColor),
+          value: isChecked,
+          onChanged: (bool? value) {
+            setState(() {
+              isChecked = value!;
+            });
+          },
+        ),
+      ]
+    )
+    );
+  }
+
+  // display a dialog for the user to enter items
+  Future<dynamic> _displayDialog(BuildContext context) async {
+    // alter the app state to show a dialog
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add a task to your list'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: const InputDecoration(hintText: 'Enter task here'),
+          ),
+          actions: <Widget>[
+            // add button
+            TextButton(
+              child: const Text('ADD'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _addTodoItem(_textFieldController.text);
+              },
+            ),
+            // Cancel button
+            TextButton(
+              child: const Text('CANCEL'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      } 
+    );
+  }
+  // iterates through our todo list title
+  List<Widget> _getItems() {
+    final List<Widget> _todoWidgets = <Widget>[];
+    for (String title in _todoList) {
+      _todoWidgets.add(_buildTodoItem(title));
+    }
+    return _todoWidgets;
+  }
+
+}
